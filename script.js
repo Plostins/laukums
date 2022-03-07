596c94416b04fc6c5fa5c3865f6f8a2a86b749b1
Plostins <plostinse.kekava@gmail.com>
2022-03-07 15:14:10 +0200

    32

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function nolasa(m1,m2,m3){
         return false;
     }
     }
-    function perimetrs(m1,m2,m3){
+function perimetrs(m1,m2,m3){
         const m = nolasa();
         m1 = m.m1; m2 = m.m2; m3 = m.m3;
         console.log(m1,m2,m3);
@@ -20,22 +20,57 @@ function nolasa(m1,m2,m3){
         console.log(p);
         return(p);
     }
-    function pusper(m1,m2,m3){
+function pusper(m1,m2,m3){
         const pusper = perimetrs(m1,m2,m3)/2;
         console.log(pusper);
         return(pusper);
     }
-    function laukums(m1,m2,m3){
+function laukums(m1,m2,m3){
         const pusper = perimetrs(m1,m2,m3) /2;
         let tlaukums = Math.sqrt(pusper*((pusper - m1)*(pusper - m2)*(pusper - m3)));
             console.log(tlaukums);
             return tlaukums;
 
     }
-    function irTrijsturis(m1,m2,m3){
+function irTrijsturis(m1,m2,m3){
     if(m1<m2+m3 && m2<m1+m3 && m3<m1+m2) {
         return true;
     }else{
         return false;
     }
+
+function rezultats(){
+        m1=m.m1; m2=m.m2; m3=m.m3;
+        console.log(m1,m2,m3);
+        if(!nolasa()){
+            t="Nekorekti ievaddati, trijstūra malu vērtībam ir jābūt lielākām par 0";
+        }else{
+            if(irTrijsturis(m1,m2,m3)==false){
+                t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+            }else{
+                t="Trijsturis ar malu garumiem "+m1+","+m2+"un"+m3+" eksistē";
+                if(m1==m2&&m2==m3){
+                    t+="Tas ir vienādmalu trijstūris."
+                }else{
+                if (m1==m2||m2==m3||m1==m3){
+                    t+="Tas ie vienadsanu trijsturis."
+                }
+            }
+const p = perimetrs(m1,m2,m3);
+const s = Math.round(laukums(m1,m2,m3) *100) / 100;
+t+="Perimetrs ir "+p+" un laukums ir "+s+".";
 }
+console.log(t);
+return t;
+    }
+function izvadaTekstu(){
+    const teksts = rezultats();
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    raksti.innerHTML = teksts;
+    sakne.appendChild(raksti);
+}
+}
+}
+
