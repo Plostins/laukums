a7c7dd70a9717e557786c95858165474389b948b
Plostins <plostinse.kekava@gmail.com>
2022-03-28 15:06:44 +0300

    1222222

--- script.js
+++ script.js
@@ -1,69 +1,73 @@
-function nolasa(m1,m2,m3){
+function nolasa(m1, m2, m3) {
     const malas = document.getElementsByClassName("mala");
-     m1 = parseFloat(mala1.value);
-     m2 = parseFloat(mala2.value);
-     m3 = parseFloat(mala3.value);
+    m1 = parseFloat(mala1.value);
+    m2 = parseFloat(mala2.value);
+    m3 = parseFloat(mala3.value);
 
-    if(m1>0 && m2>0 && m3>0){
-        console.log({m1,m2,m3});
-        return{m1,m2,m3};
-    }else{
+    if (m1 > 0 && m2 > 0 && m3 > 0) {
+        console.log({ m1, m2, m3 });
+        return { m1, m2, m3 };
+    } else {
         console.log(false);
         return false;
     }
-    }
-function perimetrs(m1,m2,m3){
-        const m = nolasa();
-        m1 = m.m1; m2 = m.m2; m3 = m.m3;
-        console.log(m1,m2,m3);
-        const p = m1+m2+m3;
-        console.log(p);
-        return(p);
-    }
-function pusper(m1,m2,m3){
-        const pusper = perimetrs(m1,m2,m3)/2;
-        console.log(pusper);
-        return(pusper);
-    }
-function laukums(m1,m2,m3){
-        const pusper = perimetrs(m1,m2,m3) /2;
-        let tlaukums = Math.sqrt(pusper*((pusper - m1)*(pusper - m2)*(pusper - m3)));
-            console.log(tlaukums);
-            return tlaukums;
+}
+function perimetrs(m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return (p);
+}
+function pusper(m1, m2, m3) {
+    const pusper = perimetrs(m1, m2, m3) / 2;
+    console.log(pusper);
+    return (pusper);
+}
+function laukums(m1, m2, m3) {
+    const pusper = perimetrs(m1, m2, m3) / 2;
+    let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
+    console.log(tlaukums);
+    return tlaukums;
 
-    }
-function irTrijsturis(m1,m2,m3){
-    if(m1<m2+m3 && m2<m1+m3 && m3<m1+m2) {
+}
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
         return true;
-    }else{
+    } else {
         return false;
     }
-
-function rezultats(){
-        m1=m.m1; m2=m.m2; m3=m.m3;
-        console.log(m1,m2,m3);
-        if(!nolasa()){
-            t="Nekorekti ievaddati, trijstūra malu vērtībam ir jābūt lielākām par 0";
-        }else{
-            if(irTrijsturis(m1,m2,m3)==false){
-                t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
-            }else{
-                t="Trijsturis ar malu garumiem "+m1+","+m2+"un"+m3+" eksistē";
-                if(m1==m2&&m2==m3){
-                    t+="Tas ir vienādmalu trijstūris."
-                }else{
-                if (m1==m2||m2==m3||m1==m3){
-                    t+="Tas ie vienadsanu trijsturis."
-                }
-            }
-const p = perimetrs(m1,m2,m3);
-const s = Math.round(laukums(m1,m2,m3) *100) / 100;
-t+="Perimetrs ir "+p+" un laukums ir "+s+".";
 }
-console.log(t);
-return t;
+
+    function rezultats() {
+        const m =nolasa();
+        m1 = m.m1; m2 = m.m2; m3 = m.m3;
+        console.log(m1, m2, m3);
+        if (!nolasa()) {
+            t = "Nekorekti ievaddati, trijstūra malu vērtībam ir jābūt lielākām par 0";
+        } else {
+            if (irTrijsturis(m1, m2, m3) == false) {
+                t = "Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+            } else {
+                t = "Trijsturis ar malu garumiem " + m1 + "," + m2 + "un" + m3 + " eksistē";
+                if (m1 == m2 && m2 == m3) {
+                    t += "Tas ir vienādmalu trijstūris."
+                } else {
+                    if (m1 == m2 || m2 == m3 || m1 == m3) {
+                        t += "Tas ie vienadsanu trijsturis."
+                    }
+                }
+                const p = perimetrs(m1, m2, m3);
+                const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
+                t += "Perimetrs ir " + p + " un laukums ir " + s + ".";
+            }
+        }
+        console.log(t);
+        return t;
     }
-function izvadaTekstu(){
+
+function izvadaTekstu() {
     const teksts = rezultats();
     console.log(teksts);
     const sakne = document.getElementById("izvade");
@@ -71,6 +75,3 @@ function izvadaTekstu(){
     raksti.innerHTML = teksts;
     sakne.appendChild(raksti);
 }
-}
-}
-
